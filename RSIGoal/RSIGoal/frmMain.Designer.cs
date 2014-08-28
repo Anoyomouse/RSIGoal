61910ed05418a393113e0e6b634c4737e1610d52
David-John Miller <anoyomouse@petermiller.co.za>
2014-08-28 10:42:02 +0200

    Initial commit, New project, etc, VS2013

---
+++ RSIGoal/RSIGoal/frmMain.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace RSIGoal
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.txtGoal = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// txtGoal
+			// 
+			this.txtGoal.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(240)))), ((int)(((byte)(255)))));
+			this.txtGoal.Location = new System.Drawing.Point(86, 12);
+			this.txtGoal.Multiline = true;
+			this.txtGoal.Name = "txtGoal";
+			this.txtGoal.Size = new System.Drawing.Size(254, 51);
+			this.txtGoal.TabIndex = 0;
+			this.txtGoal.Text = "$52,000,000";
+			this.txtGoal.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// frmMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(359, 118);
+			this.Controls.Add(this.txtGoal);
+			this.Name = "frmMain";
+			this.Text = "Form1";
+			this.Paint += new System.Windows.Forms.PaintEventHandler(this.frmMain_Paint);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+		private System.Windows.Forms.TextBox txtGoal;
+    }
+}
+
---
+++ RSIGoal/RSIGoal/frmMain.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RSIGoal.Properties;
+
+namespace RSIGoal
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+
+			this.DoubleBuffered = true;
+        }
+
+		private void frmMain_Paint(object sender, PaintEventArgs e)
+		{
+			e.Graphics.DrawImage(Resources.seriescarouselbg, 0, 0);
+
+			var bg_image = Resources.bg_large_lines;
+			var tiles_x = this.ClientSize.Width / bg_image.Width;
+			var tiles_y = this.ClientSize.Height / bg_image.Height;
+
+			for (int x = 0; x < tiles_x; x++)
+				for (int y = 0; y < tiles_y; y++)
+				{
+					e.Graphics.DrawImage(bg_image, bg_image.Width * x, bg_image.Height * y);
+				}
+		}
+
+		private void DrawTextShadow()
+		{
+			//TextRenderer.DrawText(Graphics, text, font, new Point(20,20), Color.FromArgb(0x0000f0ff);
+		}
+    }
+}
